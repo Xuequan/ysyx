253fccde6e3ead57b75a55f7f6975283d8621b88
tracer-ysyx <tracer@ysyx.org>
2023-09-14 10:01:55 +0800

    > compile NEMU ysyx_22040000 廖学全 Linux chuan 5.15.0-83-generic #92-Ubuntu SMP Mon Aug 14 09:30:42 UTC 2023 x86_64 x86_64 x86_64 GNU/Linux  10:01:55 up 2 days,  1:16,  1 user,  load average: 0.25, 0.11, 0.10

--- nemu/src/monitor/sdb/sdb.c
+++ nemu/src/monitor/sdb/sdb.c
@@ -83,7 +83,6 @@ static int cmd_info(char *args) {
 
 static int cmd_x(char *args) {
 	char *str = args;
-	char *str1 = args;
 	char *num = NULL;
 	char *addr = NULL;
 	int  whitespace_num = 0;
@@ -93,10 +92,11 @@ static int cmd_x(char *args) {
 		printf("Please input arguments, for example, 'x 10 0x8000000'\n");
 	}else {
 		// parse args
-		for (; str1++, *str++ != '\0'; ) {
+		for (; *str++ != '\0'; ) {
+		// for (; *str != '\0'; str++) {   
 			if (*str == ' ' && whitespace_num == 0) {
 				num = args;
-				*str1 = '\0';	
+				*str = '\0';	
 				whitespace_num = 1;
 			}else if (*str == ' ' && whitespace_num == 1) {
 				continue;
