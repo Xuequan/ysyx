e00e6bbf5f2586ece24dae709b46d470f07bf821
tracer-ysyx <tracer@ysyx.org>
2023-09-14 16:19:21 +0800

    > compile NEMU ysyx_22040000 廖学全 Linux chuan 5.15.0-83-generic #92-Ubuntu SMP Mon Aug 14 09:30:42 UTC 2023 x86_64 x86_64 x86_64 GNU/Linux  16:19:21 up 2 days,  7:33,  1 user,  load average: 0.56, 0.48, 0.47

--- nemu/src/monitor/sdb/expr.c
+++ nemu/src/monitor/sdb/expr.c
@@ -24,6 +24,7 @@ enum {
   TK_NOTYPE = 256, TK_EQ,
 
   /* TODO: Add more token types */
+	TK_NUM,
 
 };
 
@@ -39,6 +40,16 @@ static struct rule {
   {" +", TK_NOTYPE},    // spaces
   {"\\+", '+'},         // plus
   {"==", TK_EQ},        // equal
+
+	/* chuan start */
+	{"[0-9]+", TK_NUM},  // numbers
+	{"\\-", '-'},          // minus
+	{"\\*", '*'},					// mul
+	{"\\/", '/'},					// div
+	{"\\(", '('},					// open parenthesis	
+	{"\\)", ')'},					// close parenthesis
+	/* end */
+	
 };
 
 #define NR_REGEX ARRLEN(rules)
