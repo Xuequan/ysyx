180c080d4b80a1c393c2fbd57bd33c79f41cb46d
tracer-ysyx <tracer@ysyx.org>
2023-10-11 21:45:25 +0800

    > compile NEMU ysyx_22040000 廖学全 Linux chuan 5.15.0-84-generic #93-Ubuntu SMP Tue Sep 5 17:16:10 UTC 2023 x86_64 x86_64 x86_64 GNU/Linux  21:45:25 up 3 days, 13:09,  1 user,  load average: 0.23, 0.14, 0.10

--- nemu/src/monitor/sdb/expr.c
+++ nemu/src/monitor/sdb/expr.c
@@ -107,7 +107,7 @@ static bool make_token(char *e) {
 	
   nr_token = 0;
 	
-	//printf("make_token( %s )\n", e);
+	printf("make_token( %s )\n", e);
 
   while (e[position] != '\0') {
     /* Try all rules one by one. */
