30251e4d96af4a4a836729db23aef05f21484bcf
tracer-ysyx <tracer@ysyx.org>
2023-10-12 16:05:29 +0800

    > compile NEMU ysyx_22040000 廖学全 Linux chuan 5.15.0-84-generic #93-Ubuntu SMP Tue Sep 5 17:16:10 UTC 2023 x86_64 x86_64 x86_64 GNU/Linux  16:05:29 up 4 days,  7:29,  1 user,  load average: 0.30, 0.24, 0.24

--- nemu/src/monitor/sdb/expr.c
+++ nemu/src/monitor/sdb/expr.c
@@ -238,7 +238,7 @@ void assign_tokens_type(int type, int *index) {
 
 void print_tokens(int length) {
 	for(int i = 0; i < length; i++) {
-		printf("print_tokens--%d: tokens[%d].str = %s\n", i, i, tokens[i].str);
+		printf("print_tokens--%d: tokens[%d].type = %d, tokens[%d].str = %s\n", i, i, tokens[i].type, i, tokens[i].str);
 	}
 }
 
