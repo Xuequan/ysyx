1616d27d65c94692deaed19999730c46ec1b30c7
tracer-ysyx <tracer@ysyx.org>
2023-10-13 22:30:58 +0800

    > compile NEMU ysyx_22040000 廖学全 Linux chuan 5.15.0-84-generic #93-Ubuntu SMP Tue Sep 5 17:16:10 UTC 2023 x86_64 x86_64 x86_64 GNU/Linux  22:30:58 up 5 days, 13:54,  1 user,  load average: 0.55, 0.38, 0.43

--- nemu/src/monitor/sdb/expr.c
+++ nemu/src/monitor/sdb/expr.c
@@ -264,6 +264,26 @@ char* get_num(char c) {
 			return "14";
 		case 'f': case 'F':
 			return "15";
+		case '0': 
+			return "0";
+		case '1':
+			return "1";
+		case '2': 
+			return "2";
+		case '3':
+			return "3";
+		case '4': 
+			return "4";
+		case '5':
+			return "5";
+		case '6': 
+			return "6";
+		case '7':
+			return "7";
+		case '8': 
+			return "8";
+		case '9':
+			return "9";
 		default:
 			printf("Unknow hex digite: %c\n", c);
 			assert(0);
