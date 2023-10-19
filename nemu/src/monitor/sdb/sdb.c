2d6c7453d828fc4022552c0120d416ec221205f2
tracer-ysyx <tracer@ysyx.org>
2023-10-19 15:29:35 +0800

    > compile NEMU ysyx_22040000 廖学全 Linux chuan 5.15.0-84-generic #93-Ubuntu SMP Tue Sep 5 17:16:10 UTC 2023 x86_64 x86_64 x86_64 GNU/Linux  15:29:35 up 11 days,  6:53,  1 user,  load average: 0.60, 0.57, 0.54

--- nemu/src/monitor/sdb/sdb.c
+++ nemu/src/monitor/sdb/sdb.c
@@ -174,11 +174,7 @@ static int cmd_p(char *args) {
 				printf("expr() failed\n");
 				assert(0);
 			}
-			if (expr_result != (word_t)atoi(test_result)) {
-				/*
-				printf("expr_result = %u, atoi(test_result) = %u\n", 
-								expr_result, (word_t)atoi(test_result));
-				*/
+			if (expr_result != *(word_t *)test_result ) {
 				printf("expr_result = %d, atoi(test_result) = %d\n", 
 								(int)expr_result, (int)atoi(test_result));
 				assert(0);
