7651c74d884679b08c563aa5061cf5544310c2c1
tracer-ysyx <tracer@ysyx.org>
2024-01-31 21:45:54 +0800

    > sim RTL ysyx_22040000 廖学全 Linux chuan 5.15.0-92-generic #102-Ubuntu SMP Wed Jan 10 09:33:48 UTC 2024 x86_64 x86_64 x86_64 GNU/Linux  21:45:54 up 2 days, 14:14,  1 user,  load average: 0.14, 0.21, 0.27

--- npc/csrc/main.cpp
+++ npc/csrc/main.cpp
@@ -1,6 +1,24 @@
-#include <stdio.h>
+#include <Vtop.h>
+#include <stdlib.h>
+#include "nvboard.h"
+
+static TOP_NAME dut;
+
+void nvboard_bind_all_pins(Vtop *top) {
+	nvboard_bind_pin(&top->a, false, true, 1, LD0);
+	nvboard_bind_pin(&top->b, false, true, 1, LD0);
+	nvboard_bind_pin(&top->f, false, false, 1, LD0);
+}
 
 int main() {
-  printf("Hello, ysyx!\n");
-  return 0;
+	nvboard_bind_all_pins(&dut);
+	nvboard_init();
+	while (1) {
+		dut.a = rand() & 1;
+		dut.b = rand() & 1;
+		dut.eval();
+		nvboard_update();
+	}
+	nvboard_quit();
+	return 0;
 }
