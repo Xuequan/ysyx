e3ef6fe07c0b140a6d0dfd32de20ae0ec49bdd81
tracer-ysyx <tracer@ysyx.org>
2024-02-01 11:06:33 +0800

    > sim RTL ysyx_22040000 廖学全 Linux chuan 5.15.0-92-generic #102-Ubuntu SMP Wed Jan 10 09:33:48 UTC 2024 x86_64 x86_64 x86_64 GNU/Linux  11:06:33 up 3 days,  3:34,  1 user,  load average: 0.41, 0.55, 0.48

--- npc/csrc/main.cpp
+++ npc/csrc/main.cpp
@@ -42,7 +42,10 @@ int main() {
 	while (1) {
 		top->a = rand() & 1;
 		top->b = rand() & 1;
-		step_and_dump_wave();
+		//step_and_dump_wave();
+		top->eval();
+		contextp->timeInc(1);
+		tfp->dump(contextp->time());
 
 		nvboard_update();
 	}
