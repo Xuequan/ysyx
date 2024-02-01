f991a22adadb1fda38427c4a1d9f266c44f975ab
tracer-ysyx <tracer@ysyx.org>
2024-02-01 10:11:09 +0800

    > sim RTL ysyx_22040000 廖学全 Linux chuan 5.15.0-92-generic #102-Ubuntu SMP Wed Jan 10 09:33:48 UTC 2024 x86_64 x86_64 x86_64 GNU/Linux  10:11:09 up 3 days,  2:39,  1 user,  load average: 0.23, 0.33, 0.32

--- npc/csrc/main.cpp
+++ npc/csrc/main.cpp
@@ -1,9 +1,32 @@
-#include <Vtop.h>
+#include "Vtop.h"
 #include <stdlib.h>
 #include "nvboard.h"
+#include "verilated_vcd_c.h"
+#include "verilated.h"
 
-static TOP_NAME dut;
+// static TOP_NAME dut;
 
+VerilatedContext* contextp = NULL;
+VerilatedVcdC* tfp = NULL;
+
+static Vtop* top;
+
+void step_and_dump_wave() {
+	top->eval();
+	contextp->timeInc(1);
+	tfp->dump(contextp->time());
+}
+void sim_init() {
+	contextp = new VerilatedContext;
+	tfp = new VerilatedVcdC;
+	top = new Vtop;
+	contextp->traceEverOn(true);
+	top->trace(tfp, 0);
+	tfp->open("dump.vcd");
+}
+void sim_exit() {
+	tfp->close();
+}
 void nvboard_bind_all_pins(Vtop *top) {
 	nvboard_bind_pin(&top->a, false, true, 1, LD0);
 	nvboard_bind_pin(&top->b, false, true, 1, LD0);
@@ -11,14 +34,22 @@ void nvboard_bind_all_pins(Vtop *top) {
 }
 
 int main() {
-	nvboard_bind_all_pins(&dut);
+	sim_init();
+
+	nvboard_bind_all_pins(top);
 	nvboard_init();
-	while (1) {
-		dut.a = rand() & 1;
-		dut.b = rand() & 1;
-		dut.eval();
+	
+	int i = 0;
+	while (i< 10) {
+		top->a = rand() & 1;
+		top->b = rand() & 1;
+		step_and_dump_wave();
+
 		nvboard_update();
+		i++;
 	}
 	nvboard_quit();
+	
+	sim_exit();
 	return 0;
 }
