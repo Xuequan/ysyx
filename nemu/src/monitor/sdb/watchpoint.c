f9f1e6ccf369a5d7062978219df464b50491613a
tracer-ysyx <tracer@ysyx.org>
2024-02-05 15:17:58 +0800

    > compile NEMU ysyx_22040000 廖学全 Linux chuan 5.15.0-92-generic #102-Ubuntu SMP Wed Jan 10 09:33:48 UTC 2024 x86_64 x86_64 x86_64 GNU/Linux  15:17:58 up 2 days, 16:49,  1 user,  load average: 0.25, 0.25, 0.30

--- nemu/src/monitor/sdb/watchpoint.c
+++ nemu/src/monitor/sdb/watchpoint.c
@@ -125,7 +125,7 @@ void free_wp(int num) {
 	}
 	WP* ptr = head;
 	for ( ; ptr != NULL && ptr->NO != num; ptr = ptr->next) {
-		;
+		printf("free_wp(), ptr->NO = %d\n", ptr->NO)	;
 	}
 	if (ptr == NULL) {
 		printf("watchpoint %d is not exist\n", num);
@@ -139,7 +139,7 @@ void free_wp(int num) {
 		head = NULL;
 	} else {
 		WP* prev = head;
-		for ( ; prev->next == ptr; prev = prev->next) {	;}
+		for ( ; prev->next != ptr; prev = prev->next) {	;}
 		prev->next = ptr->next;
 		ptr->next = NULL;
 		add_wp2tail(&free_, ptr);
