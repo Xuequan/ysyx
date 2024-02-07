95196d9618aa85da90811005024d8a7fbf11a009
tracer-ysyx <tracer@ysyx.org>
2024-02-07 12:57:47 +0800

    > compile NEMU ysyx_22040000 廖学全 Linux chuan 5.15.0-92-generic #102-Ubuntu SMP Wed Jan 10 09:33:48 UTC 2024 x86_64 x86_64 x86_64 GNU/Linux  12:57:47 up 1 day,  5:19,  1 user,  load average: 0.87, 1.23, 1.88

--- nemu/src/monitor/sdb/expr.c
+++ nemu/src/monitor/sdb/expr.c
@@ -307,7 +307,7 @@ static word_t eval (int p, int q) {
 		} else if (tokens[p].type == TK_HEX){
 
 			long temp = strtol(tokens[p].str, NULL, 16);
-			printf("tokens[p].str = %s, value = %ld\n", tokens[p].str, temp);
+			printf("tokens[p].str = %s, value = %ld, val = %d\n", tokens[p].str, temp, (int)temp);
 			
 			return (word_t)strtol(tokens[p].str, NULL, 16);
 		} else {
