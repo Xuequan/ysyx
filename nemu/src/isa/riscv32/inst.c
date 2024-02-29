3cb48a293109adcdf3a7b2ff99f7b7617f09eb35
tracer-ysyx <tracer@ysyx.org>
2024-02-29 21:42:37 +0800

    > compile NEMU ysyx_22040000 廖学全 Linux chuan 5.15.0-97-generic #107-Ubuntu SMP Wed Feb 7 13:26:48 UTC 2024 x86_64 x86_64 x86_64 GNU/Linux  21:42:37 up 1 day, 14:17,  1 user,  load average: 0.11, 0.44, 1.25

--- nemu/src/isa/riscv32/inst.c
+++ nemu/src/isa/riscv32/inst.c
@@ -53,7 +53,7 @@ static void decode_operand(Decode *s, int *rd, word_t *src1, word_t *src2, word_
     case TYPE_U:                   immU(); break;
     case TYPE_S: src1R(); src2R(); immS(); break;
 		case TYPE_J: immJ(); 		 updateDnpc();break; 
-    case TYPE_I_JALR: src1R();  immI(); updateDnpc2(); /* setLSBZero(); */  break;
+    case TYPE_I_JALR: src1R();  immI(); updateDnpc2(); setLSBZero();   break;
   }
 }
 
