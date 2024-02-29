37d52e1a6036294740670e3d7a94848f13ca8fb0
tracer-ysyx <tracer@ysyx.org>
2024-02-29 15:27:42 +0800

    > compile NEMU ysyx_22040000 廖学全 Linux chuan 5.15.0-97-generic #107-Ubuntu SMP Wed Feb 7 13:26:48 UTC 2024 x86_64 x86_64 x86_64 GNU/Linux  15:27:42 up 1 day,  8:02,  1 user,  load average: 0.42, 0.31, 0.57

--- nemu/src/isa/riscv32/inst.c
+++ nemu/src/isa/riscv32/inst.c
@@ -33,7 +33,7 @@ enum {
 #define immU() do { *imm = SEXT(BITS(i, 31, 12), 20) << 12; } while(0)
 #define immS() do { *imm = (SEXT(BITS(i, 31, 25), 7) << 5) | BITS(i, 11, 7); } while(0)
 
-#define immJ() do { *imm = (SEXT(BITS(i, 31, 31), 20) << 20 ) | \
+#define immJ() do { *imm = (SEXT(BITS(i, 31, 31), 1) << 20 ) | \
 														BITS(i, 30, 21) << 1 | (BITS(i, 20, 20) << 11) | \
 													 (BITS(i, 19, 12) << 12); } while(0)
 #define updateDnpc() do { s->dnpc = s->pc + *imm; } while(0)
