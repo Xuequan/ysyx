b67db5685d3580887b4d5b4c9ff0548192e21177
tracer-ysyx <tracer@ysyx.org>
2024-03-06 10:30:14 +0800

    > sim RTL ysyx_22040000 廖学全 Linux chuan 5.15.0-97-generic #107-Ubuntu SMP Wed Feb 7 13:26:48 UTC 2024 x86_64 x86_64 x86_64 GNU/Linux  10:30:14 up  3:18,  1 user,  load average: 0.09, 0.07, 0.10

--- npc/vsrc/ysyx_23060208_PCreg.v
+++ npc/vsrc/ysyx_23060208_PCreg.v
@@ -10,21 +10,13 @@ module ysyx_23060208_PCreg
 	output reg [ADDR_WIDTH-1:0] pc
 );
 
-//Reg #(.WIDTH(ADDR_WIDTH), .RESET_VAL(ADDR_WIDTH'('h8000_0000))) reg_i0(
-Reg #(.WIDTH(ADDR_WIDTH), .RESET_VAL(32'h8000_0000)) reg_i0(
+Reg #(.WIDTH(ADDR_WIDTH), .RESET_VAL(ADDR_WIDTH'('h8000_0000))) reg_i0(
+// Reg #(.WIDTH(ADDR_WIDTH), .RESET_VAL(32'h8000_0000)) reg_i0(
 	.clk(clk),
 	.rst(rst),
 	.din(next_pc),
 	.dout(pc),
-	.wen(wen_r)   
+	.wen(1'b1)   
 );
-
-reg wen_r;
-always @(posedge clk) begin
-	if (rst)
-		wen_r <= 0;
-	else 
-		wen_r <= 1;
-end
 
 endmodule
