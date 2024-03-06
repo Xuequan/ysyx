f19fdb25e4ef911c286a595f3e64425f724de237
tracer-ysyx <tracer@ysyx.org>
2024-03-06 10:43:19 +0800

    > sim RTL ysyx_22040000 廖学全 Linux chuan 5.15.0-97-generic #107-Ubuntu SMP Wed Feb 7 13:26:48 UTC 2024 x86_64 x86_64 x86_64 GNU/Linux  10:43:19 up  3:31,  1 user,  load average: 0.01, 0.12, 0.14

--- npc/vsrc/ysyx_23060208_IFU.v
+++ npc/vsrc/ysyx_23060208_IFU.v
@@ -6,24 +6,17 @@ module ysyx_23060208_IFU
 	input [DATA_WIDTH-1:0] inst_i,  // from inst ram
 
 	//output reg [ADDR_WIDTH-1:0] addr_o // to inst ram
-	output [ADDR_WIDTH-1:0] addr
+	output reg [ADDR_WIDTH-1:0] addr    // to inst ram
 );
 
 wire [ADDR_WIDTH-1:0] next_pc;  // to IDU
-wire [ADDR_WIDTH-1:0] addr_o;
-assign next_pc = addr_o + ADDR_WIDTH'('h4);
+assign next_pc = addr + ADDR_WIDTH'('h4);
 
 ysyx_23060208_PCreg #(.ADDR_WIDTH(ADDR_WIDTH)) PCreg_i0(
 	.clk(clk),
 	.rst(rst),
 	.next_pc(next_pc),
-	.pc(addr_o)
+	.pc(addr)
 );
-
-reg [ADDR_WIDTH-1:0] addr_r; 
-always @(posedge clk) begin
-		addr_r <= addr_o;
-end
-assign addr = addr_r;
 
 endmodule
