58a84e81e0b28fd44591c8f90b7bdf20c790d6f7
tracer-ysyx <tracer@ysyx.org>
2024-03-14 13:36:02 +0800

    > compile NEMU ysyx_22040000 廖学全 Linux chuan 5.15.0-100-generic #110-Ubuntu SMP Wed Feb 7 13:27:48 UTC 2024 x86_64 x86_64 x86_64 GNU/Linux  13:36:02 up 3 days,  6:16,  1 user,  load average: 0.12, 0.16, 0.45

--- nemu/src/cpu/cpu-exec.c
+++ nemu/src/cpu/cpu-exec.c
@@ -153,7 +153,7 @@ void cpu_exec(uint64_t n) {
            (nemu_state.halt_ret == 0 ? ANSI_FMT("HIT GOOD TRAP", ANSI_FG_GREEN) :
             													 ANSI_FMT("HIT BAD TRAP", ANSI_FG_RED))),
           nemu_state.halt_pc);
-			if (nemu_state.halt_ret == 0)
+			if (nemu_state.halt_ret != 0)
 				print_iringbuf();
       // fall through
     case NEMU_QUIT: statistic(); 
