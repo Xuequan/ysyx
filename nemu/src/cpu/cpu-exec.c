bd527b22b73ba2bd053078ab36578064fd90e458
tracer-ysyx <tracer@ysyx.org>
2024-03-18 15:30:46 +0800

    > compile NEMU ysyx_22040000 廖学全 Linux chuan 5.15.0-100-generic #110-Ubuntu SMP Wed Feb 7 13:27:48 UTC 2024 x86_64 x86_64 x86_64 GNU/Linux  15:30:46 up 3 days,  8:09,  1 user,  load average: 0.68, 0.73, 0.77

--- nemu/src/cpu/cpu-exec.c
+++ nemu/src/cpu/cpu-exec.c
@@ -58,7 +58,7 @@ static void trace_and_difftest(Decode *_this, vaddr_t dnpc) {
 #endif
   if (g_print_step) { IFDEF(CONFIG_ITRACE, puts(_this->logbuf)); }
   IFDEF(CONFIG_DIFFTEST, difftest_step(_this->pc, dnpc));
-	// chuan add
+	// chceck if reach breakpoint
 	scan_wp_pool();
 }
 
