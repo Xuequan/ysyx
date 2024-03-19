bbc7987630ce0ed0d89e9eb648ec77743f5c84a1
tracer-ysyx <tracer@ysyx.org>
2024-03-19 14:02:19 +0800

    > compile NEMU ysyx_22040000 廖学全 Linux chuan 5.15.0-100-generic #110-Ubuntu SMP Wed Feb 7 13:27:48 UTC 2024 x86_64 x86_64 x86_64 GNU/Linux  14:02:19 up  8:31,  1 user,  load average: 0.53, 0.36, 0.43

--- nemu/src/cpu/cpu-exec.c
+++ nemu/src/cpu/cpu-exec.c
@@ -127,15 +127,15 @@ static void exec_once(Decode *s, vaddr_t pc) {
 		printf("%#x: ----------- ret [%s]\n", s->pc, now_func);
 	}
 	*/
-	if (identify_inst(s->pc, s->isa.inst.val) == 1)  { 
+	int ident = identify_inst(s->pc, s->isa.inst.val);
+	if (1 == ident)  { 
 		next_func = vaddr2func(s->dnpc, &success2, 1); 
 		if (success2){
 			printf("%#x: call [%s@%#x]\n", s->pc, next_func, s->dnpc);
 		}else{
 			printf("Get func name of pc: '%#x' error!\n", s->dnpc);
 		}
-	} 
-	if (identify_inst(s->pc, s->isa.inst.val) == 2){ 
+	}else if(ident == 2){ 
 		now_func  = vaddr2func(s->pc,   &success1, 0); 
 		if (success1){
 			printf("%#x: ----------- ret [%s]\n", s->pc, now_func);
@@ -143,6 +143,7 @@ static void exec_once(Decode *s, vaddr_t pc) {
 			printf("Get func name of pc: '%#x' error!\n", s->pc);
 		}
 	}
+	
 	/* ftrace end */
 
   cpu.pc = s->dnpc;
