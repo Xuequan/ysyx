df9696ead53649d41c6385225872bc677b57b49b
tracer-ysyx <tracer@ysyx.org>
2024-03-21 22:17:42 +0800

    > compile NEMU ysyx_22040000 廖学全 Linux chuan 5.15.0-101-generic #111-Ubuntu SMP Tue Mar 5 20:16:58 UTC 2024 x86_64 x86_64 x86_64 GNU/Linux  22:17:42 up 16:43,  1 user,  load average: 0.10, 0.14, 0.11

--- nemu/src/isa/riscv32/difftest/dut.c
+++ nemu/src/isa/riscv32/difftest/dut.c
@@ -15,7 +15,7 @@
 
 #include <isa.h>
 #include <cpu/difftest.h>
-#include "../local-include/reg.h"
+//#include "../local-include/reg.h"
 
 /* 对比一致则返回 true */ 
 bool isa_difftest_checkregs(CPU_state *ref_r, vaddr_t pc) {
