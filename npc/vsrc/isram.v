4b3dc8cbd1194d56465586ec19cb8dd6fbb5e5ba
tracer-ysyx <tracer@ysyx.org>
2024-03-22 09:33:45 +0800

    > sim RTL ysyx_22040000 廖学全 Linux chuan 5.15.0-101-generic #111-Ubuntu SMP Tue Mar 5 20:16:58 UTC 2024 x86_64 x86_64 x86_64 GNU/Linux  09:33:45 up 1 day,  3:59,  1 user,  load average: 0.22, 0.23, 0.16

--- npc/vsrc/isram.v
+++ npc/vsrc/isram.v
@@ -8,7 +8,7 @@ module isram
 	output reg [DATA_WIDTH-1:0] inst_o
 );
 reg [DATA_WIDTH-1:0] isram [1024:0];  // 暂定1024行
-initial $readmemb("/home/chuan/ysyx-workbench/npc/vsrc/dummy-riscv32e-npc.bin", isram);
+initial $readmemh("/home/chuan/ysyx-workbench/npc/vsrc/dummy-riscv32e-npc.bin", isram);
 
 always @(posedge clk) begin
 	if (rst) 
