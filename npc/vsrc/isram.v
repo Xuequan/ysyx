29fb3596299572b7310075ebe0b7ad7ad832ef35
tracer-ysyx <tracer@ysyx.org>
2024-03-23 09:38:26 +0800

    > sim RTL ysyx_22040000 廖学全 Linux chuan 5.15.0-101-generic #111-Ubuntu SMP Tue Mar 5 20:16:58 UTC 2024 x86_64 x86_64 x86_64 GNU/Linux  09:38:26 up  1:57,  1 user,  load average: 0.08, 0.08, 0.08

--- npc/vsrc/isram.v
+++ npc/vsrc/isram.v
@@ -22,6 +22,7 @@ always @(posedge clk) begin
 	end else begin
 		inst_o <= {isram[addr+3], isram[addr+2], isram[addr+1], isram[addr]};
 		$display("here %d", inst_o);
+		$display("addr %d", addr);
 		//inst_o <= isram[addr];
 	end
 end
