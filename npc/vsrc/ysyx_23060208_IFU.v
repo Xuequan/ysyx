44143fb371efa654c0b2c2b95e6f2d3b3c5a7d29
tracer-ysyx <tracer@ysyx.org>
2024-03-25 06:02:55 +0800

    > sim RTL ysyx_22040000 廖学全 Linux chuan 5.15.0-101-generic #111-Ubuntu SMP Tue Mar 5 20:16:58 UTC 2024 x86_64 x86_64 x86_64 GNU/Linux  06:02:55 up 1 day, 22:22,  1 user,  load average: 0.65, 0.38, 0.27

--- npc/vsrc/ysyx_23060208_IFU.v
+++ npc/vsrc/ysyx_23060208_IFU.v
@@ -16,6 +16,55 @@ module ysyx_23060208_IFU
 );
 
 reg [ADDR_WIDTH-1:0] next_pc; 
+
+parameter [1:0] IDLE = 2'b00,
+								NORMAL = 2'b01,
+								JUMP   = 2'b10;
+reg [1:0] state, next_state;
+ 
+always @(posedge clk) begin
+	if (rst) 
+		state <= IDLE;
+	else
+		state <= next_state;
+end
+always @(state or inst_jal_jalr) begin
+	next_state = NORMAL;
+	case (state)
+		IDLE: if (inst_jal_jalr) 
+						next_state = JUMP;
+					else
+						next_state = NORMAL;
+		NORMAL: if (inst_jal_jalr)
+						next_state = JUMP;
+					else
+						next_state = NORMAL;
+		JUMP: if (inst_jal_jalr)
+						next_state = JUMP;
+					else
+						next_state = NORMAL;
+		default:;
+	endcase
+end
+
+always @(posedge clk) begin
+	if (rst) 
+		next_pc <= ADDR_WIDTH'('h8000_0000);
+	else begin
+		next_pc <= ADDR_WIDTH'('h8000_0000);
+		case (next_state)
+			IDLE:;
+			NORMAL: 
+				next_pc <= next_pc + ADDR_WIDTH'('h4);
+			JUMP:
+				next_pc <= nextpc_from_jal_jalr;
+			default:;
+		endcase
+	end
+end	
+						
+			
+/*
 always @(posedge clk) begin
 	if (rst) 
 		next_pc <= ADDR_WIDTH'('h8000_0000);
@@ -24,6 +73,7 @@ always @(posedge clk) begin
 	else
 		next_pc <= next_pc + ADDR_WIDTH'('h4);
 end
+*/
 
 ysyx_23060208_PC #(.ADDR_WIDTH(ADDR_WIDTH)) PC_i0(
 	.clk(clk),
