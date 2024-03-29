39d6c1a4550a56fdab3f380de32e1da1caff8672
tracer-ysyx <tracer@ysyx.org>
2024-03-29 15:35:16 +0800

    > sim RTL ysyx_22040000 廖学全 Linux chuan 5.15.0-101-generic #111-Ubuntu SMP Tue Mar 5 20:16:58 UTC 2024 x86_64 x86_64 x86_64 GNU/Linux  15:35:16 up 6 days,  7:54,  1 user,  load average: 0.18, 0.14, 0.10

--- npc/vsrc/ysyx_23060208_isram.v
+++ npc/vsrc/ysyx_23060208_isram.v
@@ -24,6 +24,7 @@ assign inst_o = rdata;
 always @(posedge clk) begin
 	if (rst) begin
 		rdata <= 0;
+		$display("here in isram.v rst, raddr = %x", raddr);
 	end 
 	else if (valid) begin  // 有读写请求时
 		$display("here in isram.v raddr = %x", raddr);
