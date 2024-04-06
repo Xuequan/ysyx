2b9ebd07108c2aa6fe2c76636b45733e48ac29db
tracer-ysyx <tracer@ysyx.org>
2024-04-06 08:30:22 +0800

    > sim RTL ysyx_22040000 廖学全 Linux chuan 5.15.0-101-generic #111-Ubuntu SMP Tue Mar 5 20:16:58 UTC 2024 x86_64 x86_64 x86_64 GNU/Linux  08:30:22 up  1:01,  1 user,  load average: 0.29, 0.15, 0.14

--- npc/discard/csrc/common2.h
+++ npc/discard/csrc/common2.h
@@ -103,7 +103,7 @@ uint64_t get_time();
 
 #define ANSI_FMT(str, fmt) fmt str ANSI_NONE
 
-#define log_write(...) IFDEF(CONFIG_TARGET_NATIVE_ELF, \
+#define log_write(...) \
   do { \
     extern FILE* log_fp; \
     extern bool log_enable(); \
@@ -111,8 +111,7 @@ uint64_t get_time();
       fprintf(log_fp, __VA_ARGS__); \
       fflush(log_fp); \
     } \
-  } while (0) \
-)
+  } while (0) 
 
 #define _Log(...) \
   do { \
