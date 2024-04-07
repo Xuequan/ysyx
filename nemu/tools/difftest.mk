ec49a3f7026476180a6d9f13c79d8ce849207bc0
tracer-ysyx <tracer@ysyx.org>
2024-04-07 21:50:28 +0800

    > compile NEMU ysyx_22040000 廖学全 Linux chuan 5.15.0-101-generic #111-Ubuntu SMP Tue Mar 5 20:16:58 UTC 2024 x86_64 x86_64 x86_64 GNU/Linux  21:50:28 up 1 day, 14:21,  1 user,  load average: 0.19, 0.15, 0.17

--- nemu/tools/difftest.mk
+++ nemu/tools/difftest.mk
@@ -14,6 +14,7 @@
 #**************************************************************************************/
 
 ifdef CONFIG_DIFFTEST
+$(shell echo " =============in tools/difftest.mk")
 DIFF_REF_PATH = $(NEMU_HOME)/$(call remove_quote,$(CONFIG_DIFFTEST_REF_PATH))
 DIFF_REF_SO = $(DIFF_REF_PATH)/build/$(GUEST_ISA)-$(call remove_quote,$(CONFIG_DIFFTEST_REF_NAME))-so
 MKFLAGS = GUEST_ISA=$(GUEST_ISA) SHARE=1 ENGINE=interpreter
@@ -21,6 +22,7 @@ ARGS_DIFF = --diff=$(DIFF_REF_SO)
 
 ifndef CONFIG_DIFFTEST_REF_NEMU
 $(DIFF_REF_SO):
+	@echo =============in tools/difftest.mk
 	@echo $(MAKE) -s -C $(DIFF_REF_PATH) $(MKFLAGS)
 	$(MAKE) -s -C $(DIFF_REF_PATH) $(MKFLAGS)
 endif
