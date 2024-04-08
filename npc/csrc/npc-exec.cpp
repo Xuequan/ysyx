d2755f64e8ab4fc33ca9d6147f4673a6e1186d68
tracer-ysyx <tracer@ysyx.org>
2024-04-08 15:49:48 +0800

    > compile NEMU ysyx_22040000 廖学全 Linux chuan 5.15.0-101-generic #111-Ubuntu SMP Tue Mar 5 20:16:58 UTC 2024 x86_64 x86_64 x86_64 GNU/Linux  15:49:48 up 2 days,  8:20,  1 user,  load average: 0.63, 0.73, 0.61

--- npc/csrc/npc-exec.cpp
+++ npc/csrc/npc-exec.cpp
@@ -188,14 +188,15 @@ void npc_exec(uint64_t n) {
 			npc_state.state = NPC_STOP; break;
 
 	    case NPC_END: case NPC_ABORT:
+			{
+      if (npc_state.halt_ret != 0)
+        print_iringbuf();
       Log("npc: %s at pc = " FMT_WORD,
           (npc_state.state == NPC_ABORT ? ANSI_FMT("ABORT", ANSI_FG_RED) :
            (npc_state.halt_ret == 0 ? ANSI_FMT("HIT GOOD TRAP", ANSI_FG_GREEN) :
                                        ANSI_FMT("HIT BAD TRAP", ANSI_FG_RED))),
           npc_state.halt_pc);
-
-      if (npc_state.halt_ret != 0)
-        print_iringbuf();
+			 }
       // fall through
     	case NPC_QUIT: ; statistic(); 
   	}
