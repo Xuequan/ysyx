36240a4669d396c734df10e886319021b49aed0d
tracer-ysyx <tracer@ysyx.org>
2024-04-08 15:40:23 +0800

    > compile NEMU ysyx_22040000 廖学全 Linux chuan 5.15.0-101-generic #111-Ubuntu SMP Tue Mar 5 20:16:58 UTC 2024 x86_64 x86_64 x86_64 GNU/Linux  15:40:23 up 2 days,  8:11,  1 user,  load average: 0.64, 0.53, 0.47

--- nemu/src/cpu/difftest/ref.c
+++ nemu/src/cpu/difftest/ref.c
@@ -45,7 +45,7 @@ __EXPORT void difftest_regcpy(void *dut, bool direction) {
 
 __EXPORT void difftest_exec(uint64_t n) {
 	printf("here in difftest_exec\n");
-	//cpu_exec(n);
+	cpu_exec(n);
 }
 
 __EXPORT void difftest_raise_intr(word_t NO) {
