1cadb38a43a96e8e14c02806863ea395cc0c9663
tracer-ysyx <tracer@ysyx.org>
2024-04-08 12:46:53 +0800

    > sim RTL ysyx_22040000 廖学全 Linux chuan 5.15.0-101-generic #111-Ubuntu SMP Tue Mar 5 20:16:58 UTC 2024 x86_64 x86_64 x86_64 GNU/Linux  12:46:53 up 2 days,  5:17,  1 user,  load average: 0.69, 0.40, 0.63

--- npc/csrc/difftest.cpp
+++ npc/csrc/difftest.cpp
@@ -59,7 +59,7 @@ void init_difftest(char *ref_so_file, long img_size, int port) {
       "This will help you a lot for debugging, but also significantly reduce the performance. "
       "If it is not necessary, you can turn it off in menuconfig.", ref_so_file);
 
-  //ref_difftest_init(port);
+  ref_difftest_init(port);
   ref_difftest_memcpy(RESET_VECTOR, guest_to_host(RESET_VECTOR), img_size, DIFFTEST_TO_REF);
 	printf("after difftest_memcpy\n");
 
@@ -83,6 +83,7 @@ void difftest_step() {
 	printf("before ref_difftest_exec(1)\n");
   ref_difftest_exec(1);
 	printf("after ref_difftest_exec(1)\n");
+
 	// 得到 ref 的 regs
 	uint32_t ref_regs[16] = {0};
   ref_difftest_regcpy(ref_regs, DIFFTEST_TO_DUT);
