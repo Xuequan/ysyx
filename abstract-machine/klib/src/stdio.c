2d55eb1bc975c49ac8c01a541c13a4cfde8bf52c
tracer-ysyx <tracer@ysyx.org>
2024-04-19 10:07:25 +0800

    > compile NEMU ysyx_22040000 廖学全 Linux chuan 5.15.0-102-generic #112-Ubuntu SMP Tue Mar 5 16:50:32 UTC 2024 x86_64 x86_64 x86_64 GNU/Linux  10:07:25 up 5 days,  1:02,  1 user,  load average: 0.33, 0.20, 0.31

--- abstract-machine/klib/src/stdio.c
+++ abstract-machine/klib/src/stdio.c
@@ -23,6 +23,7 @@
 #define FLAGS_ADAPT_EXP (1U << 11U)
 
 #define PRINTF_NTOA_BUFFER_SIZE 32U
+
 // output function type
 typedef void (*out_fct_type)(char character, void* buffer, size_t idx, size_t maxlen);
 
