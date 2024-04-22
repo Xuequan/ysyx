ca13bbe8dbff71144e39a7fcb886ea9d1feb5204
tracer-ysyx <tracer@ysyx.org>
2024-04-22 09:48:25 +0800

    > compile NEMU ysyx_22040000 廖学全 Linux chuan 5.15.0-102-generic #112-Ubuntu SMP Tue Mar 5 16:50:32 UTC 2024 x86_64 x86_64 x86_64 GNU/Linux  09:48:25 up 8 days, 43 min,  1 user,  load average: 0.15, 0.28, 0.33

--- abstract-machine/am/src/platform/nemu/ioe/input.c
+++ abstract-machine/am/src/platform/nemu/ioe/input.c
@@ -5,7 +5,6 @@
 
 void __am_input_keybrd(AM_INPUT_KEYBRD_T *kbd) {
 	uint32_t key = inb(KBD_ADDR);
-  kbd->keydown = key ? true : false;
-  //kbd->keycode = key & 0xff;
-  kbd->keycode = key;
+  kbd->keydown = key & KEYDOWN_MASK;
+  kbd->keycode = key & 0xff;
 }
