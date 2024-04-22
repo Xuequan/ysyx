eef6b0f68b54d8a1d4f76bc27e978f5c12d78b97
tracer-ysyx <tracer@ysyx.org>
2024-04-22 09:22:06 +0800

    > compile NEMU ysyx_22040000 廖学全 Linux chuan 5.15.0-102-generic #112-Ubuntu SMP Tue Mar 5 16:50:32 UTC 2024 x86_64 x86_64 x86_64 GNU/Linux  09:22:06 up 8 days, 16 min,  1 user,  load average: 0.12, 0.23, 0.32

--- nemu/src/device/io/map.c
+++ nemu/src/device/io/map.c
@@ -48,6 +48,9 @@ static void invoke_callback(io_callback_t c, paddr_t offset, int len, bool is_wr
 
 void init_map() {
   io_space = malloc(IO_SPACE_MAX);
+	/* try */
+	memset(io_space, '\0', IO_SPACE_MAX);
+
   assert(io_space);
   p_space = io_space;
 }
