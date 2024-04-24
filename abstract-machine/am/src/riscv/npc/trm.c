691457909322007bebfe59439b6c469ecbf58ce9
tracer-ysyx <tracer@ysyx.org>
2024-04-25 07:44:38 +0800

    > compile NEMU ysyx_22040000 廖学全 Linux chuan 5.15.0-102-generic #112-Ubuntu SMP Tue Mar 5 16:50:32 UTC 2024 x86_64 x86_64 x86_64 GNU/Linux  07:44:38 up 10 days, 22:39,  1 user,  load average: 0.38, 0.48, 1.13

--- abstract-machine/am/src/riscv/npc/trm.c
+++ abstract-machine/am/src/riscv/npc/trm.c
@@ -1,13 +1,8 @@
 #include <am.h>
-#include <klib-macros.h>
-#include <klib.h>
+#include <npc.h>
 
 extern char _heap_start;
 int main(const char *args);
-
-extern char _pmem_start;
-#define PMEM_SIZE (128 * 1024 * 1024)
-#define PMEM_END  ((uintptr_t)&_pmem_start + PMEM_SIZE)
 
 Area heap = RANGE(&_heap_start, PMEM_END);
 #ifndef MAINARGS
@@ -16,11 +11,9 @@ Area heap = RANGE(&_heap_start, PMEM_END);
 static const char mainargs[] = MAINARGS;
 
 void putch(char ch) {
-	printf("here in npc-trm.c \n");
+	outb(SERIAL_PORT, ch);
 }
 
-// 实际上的 npc_trap() 还未实现，现在只不过是遇到 ebreak 就停止了罢了
-#define npc_trap(code) asm volatile("ebreak")
 void halt(int code) {
 	npc_trap(code);
 
