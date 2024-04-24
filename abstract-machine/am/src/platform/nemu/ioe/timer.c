e4d89f9d4929154833d84d232e5e1c80d75461ff
tracer-ysyx <tracer@ysyx.org>
2024-04-24 10:33:25 +0800

    > compile NEMU ysyx_22040000 廖学全 Linux chuan 5.15.0-102-generic #112-Ubuntu SMP Tue Mar 5 16:50:32 UTC 2024 x86_64 x86_64 x86_64 GNU/Linux  10:33:25 up 10 days,  1:28,  1 user,  load average: 0.01, 0.11, 0.16

--- abstract-machine/am/src/platform/nemu/ioe/timer.c
+++ abstract-machine/am/src/platform/nemu/ioe/timer.c
@@ -13,19 +13,14 @@ void __am_timer_uptime(AM_TIMER_UPTIME_T *uptime) {
 	uint64_t tmp = 0;
 	memcpy(&tmp, start_time, 2 * sizeof(start_time[0]) );
 	uptime->us = tmp;
-	printf("uptime->us = %#lx\n", uptime->us);
+	//printf("uptime->us = %#lx\n", uptime->us);
 }
 /*
-IOMap* fetch_mmio_map(paddr_t addr);
-word_t map_read(paddr_t addr, int len, IOMap *map);
+#include <memory/paddr.h>
 void __am_timer_uptime(AM_TIMER_UPTIME_T *uptime) {
-	IOMAP *map = fetch_mmio_map(RTC_ADDR);
-	if (map == NULL) {
-		panic("__am_timer_uptime(): cannot get map");
-	}
-	word_t start_time[2] = {0};
-	word_t timer[0] = map_read(RTC_ADDR, 4, map);		 
-	word_t timer[1] = map_read(RTC_ADDR + 4, 4, map);		 
+	uint32_t start_time[2] = {0};
+	start_time[0] = paddr_read(RTC_ADDR, 4);	
+	start_time[1] = paddr_read(RTC_ADDR + 4, 4);		
 	uint64_t tmp = 0;
 	memcpy(&tmp, start_time, 2 * sizeof(start_time[0]) );
 	uptime->us = tmp;
--- abstract-machine/am/src/riscv/riscv.h
+++ abstract-machine/am/src/riscv/riscv.h
@@ -2,12 +2,15 @@
 #define RISCV_H__
 
 #include <stdint.h>
-#include <stdio.h>
+//#include <device/map.h>
 
+// 我觉得应该更改这里！！
+// 通过 map_read(), map_write() 来读/写
 static inline uint8_t  inb(uintptr_t addr) { return *(volatile uint8_t  *)addr; }
 static inline uint16_t inw(uintptr_t addr) { return *(volatile uint16_t *)addr; }
 static inline uint32_t inl(uintptr_t addr) { return *(volatile uint32_t *)addr; }
 
+/* 从 AM --> NEMU  */
 static inline void outb(uintptr_t addr, uint8_t  data) { *(volatile uint8_t  *)addr = data; }
 static inline void outw(uintptr_t addr, uint16_t data) { *(volatile uint16_t *)addr = data; }
 static inline void outl(uintptr_t addr, uint32_t data) { *(volatile uint32_t *)addr = data; }
