c7fcc50b73091a7720ccf8a09c4b7f2a62aab4da
tracer-ysyx <tracer@ysyx.org>
2024-04-24 11:44:42 +0800

    > compile NEMU ysyx_22040000 廖学全 Linux chuan 5.15.0-102-generic #112-Ubuntu SMP Tue Mar 5 16:50:32 UTC 2024 x86_64 x86_64 x86_64 GNU/Linux  11:44:42 up 10 days,  2:39,  1 user,  load average: 1.17, 1.27, 1.52

--- abstract-machine/am/src/platform/nemu/ioe/timer.c
+++ abstract-machine/am/src/platform/nemu/ioe/timer.c
@@ -2,7 +2,7 @@
 #include <nemu.h>
 #include <string.h>
 
-#include <stdio.h>
+//#include <stdio.h>
 void __am_timer_init() {
 	
 }
@@ -14,7 +14,7 @@ void __am_timer_uptime(AM_TIMER_UPTIME_T *uptime) {
 	uint64_t tmp = 0;
 	memcpy(&tmp, start_time, 2 * sizeof(start_time[0]) );
 	uptime->us = tmp;
-	printf("uptime->us = %#lx\n", uptime->us);
+	//printf("uptime->us = %#lx\n", uptime->us);
 }
 /*
 #include <memory/paddr.h>
