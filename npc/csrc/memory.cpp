6097659a455c641f7986208b80c45940c1f703d5
tracer-ysyx <tracer@ysyx.org>
2024-04-25 12:49:08 +0800

    > sim RTL ysyx_22040000 廖学全 Linux chuan 5.15.0-102-generic #112-Ubuntu SMP Tue Mar 5 16:50:32 UTC 2024 x86_64 x86_64 x86_64 GNU/Linux  12:49:08 up 11 days,  3:43,  1 user,  load average: 0.36, 0.31, 0.18

--- npc/csrc/memory.cpp
+++ npc/csrc/memory.cpp
@@ -88,8 +88,6 @@ void paddr_write(paddr_t addr, int len, word_t data) {
 		return; 
 	}
 
-	printf("==========================addr = %#x, SERIAL=%#x\n", addr, (uint32_t)(SERIAL_PORT));
-
 	if (addr == (uint32_t)(SERIAL_PORT) ) {
 		// 若是外设，则让 ref 跳过
 		difftest_skip_ref();
@@ -111,9 +109,6 @@ void paddr_write(paddr_t addr, int len, word_t data) {
 			printf("paddr_write(): len = %d is wrong\n", len);
 			return;
 		}
-	}else {
-		printf(" not equal here, addr = %#x, SERIAL=%#x\n", addr, (uint32_t)(SERIAL_PORT) );
-		return;
 	}
   out_of_bound(addr);
 }
