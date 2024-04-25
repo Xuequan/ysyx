4baded919b0f99bddd1d198a6ec5a15feeb92075
tracer-ysyx <tracer@ysyx.org>
2024-04-25 10:56:46 +0800

    > compile NEMU ysyx_22040000 廖学全 Linux chuan 5.15.0-102-generic #112-Ubuntu SMP Tue Mar 5 16:50:32 UTC 2024 x86_64 x86_64 x86_64 GNU/Linux  10:56:46 up 11 days,  1:51,  1 user,  load average: 0.15, 0.23, 0.20

--- nemu/src/cpu/cpu-exec.c
+++ nemu/src/cpu/cpu-exec.c
@@ -49,6 +49,27 @@ static void print_iringbuf(void) {
 		}
 	}
 }
+
+static void trace_and_difftest(Decode *_this, vaddr_t dnpc) {
+	// print inst information
+	// eg: 0x80000000: 00 00 02 97 auipc	t0, 0
+#ifdef CONFIG_ITRACE_COND
+  if (ITRACE_COND) { log_write("%s\n", _this->logbuf); }
+	
+#endif
+  if (g_print_step) { IFDEF(CONFIG_ITRACE, puts(_this->logbuf)); }
+  IFDEF(CONFIG_DIFFTEST, difftest_step(_this->pc, dnpc));
+	// check if reach breakpoint
+	scan_wp_pool();
+}
+
+
+#ifdef CONFIG_DIFFTEST 
+
+void vaddr2func(vaddr_t addr, bool *success, int choose, char* func_name, int len);
+#define FUNC_NAME_LEN 102
+static int space = 4;
+
 /* return 1 if function call
 ** return 2 if function ret 
 ** else return 0.
@@ -84,24 +105,7 @@ static int identify_inst(vaddr_t pc, word_t inst) {
   }
   return 0;
 }      
-
-static void trace_and_difftest(Decode *_this, vaddr_t dnpc) {
-	// print inst information
-	// eg: 0x80000000: 00 00 02 97 auipc	t0, 0
-#ifdef CONFIG_ITRACE_COND
-  if (ITRACE_COND) { log_write("%s\n", _this->logbuf); }
-	
 #endif
-  if (g_print_step) { IFDEF(CONFIG_ITRACE, puts(_this->logbuf)); }
-  IFDEF(CONFIG_DIFFTEST, difftest_step(_this->pc, dnpc));
-	// check if reach breakpoint
-	scan_wp_pool();
-}
-
-/* max function name in ELF string table */
-#define FUNC_NAME_LEN 102
-void vaddr2func(vaddr_t addr, bool *success, int choose, char* func_name, int len);
-static int space = 4;
 
 static void exec_once(Decode *s, vaddr_t pc) {
   s->pc = pc;
