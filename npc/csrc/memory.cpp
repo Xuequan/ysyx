2ac1f515f170143ecc78a953f47261318b59a58a
tracer-ysyx <tracer@ysyx.org>
2024-04-25 13:02:40 +0800

    > sim RTL ysyx_22040000 廖学全 Linux chuan 5.15.0-102-generic #112-Ubuntu SMP Tue Mar 5 16:50:32 UTC 2024 x86_64 x86_64 x86_64 GNU/Linux  13:02:40 up 11 days,  3:57,  1 user,  load average: 0.39, 0.33, 0.28

--- npc/csrc/memory.cpp
+++ npc/csrc/memory.cpp
@@ -15,7 +15,6 @@
 #include "common2.h"
 #include "arch.h"
 #include "memory.h"
-#include <cstdio>
 
 uint8_t* guest_to_host(paddr_t paddr) { return pmem + paddr - CONFIG_MBASE; }
 paddr_t host_to_guest(uint8_t *haddr) { return haddr - pmem + CONFIG_MBASE; }
