6b3679e0428a2e33d3a9e43efc6decdd5ef42fae
tracer-ysyx <tracer@ysyx.org>
2024-05-08 20:24:33 +0800

    > compile NEMU ysyx_22040000 廖学全 Linux chuan 5.15.0-105-generic #115-Ubuntu SMP Mon Apr 15 09:52:04 UTC 2024 x86_64 x86_64 x86_64 GNU/Linux  20:24:33 up 13:22,  1 user,  load average: 0.59, 0.60, 0.59

--- nemu/src/cpu/difftest/dut.c
+++ nemu/src/cpu/difftest/dut.c
@@ -110,6 +110,7 @@ void difftest_step(vaddr_t pc, vaddr_t npc) {
     ref_difftest_regcpy(&ref_r, DIFFTEST_TO_DUT);
     if (ref_r.pc == npc) {
       skip_dut_nr_inst = 0;
+			printf("pc = %#x, npc = %#x\n", pc, npc);
       checkregs(&ref_r, npc);
       return;
     }
