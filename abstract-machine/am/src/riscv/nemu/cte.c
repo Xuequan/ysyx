bb92b353d58d1795e5242cfc099cf44feefbff0d
tracer-ysyx <tracer@ysyx.org>
2024-05-15 21:37:52 +0800

    > compile NEMU ysyx_22040000 廖学全 Linux chuan 5.15.0-105-generic #115-Ubuntu SMP Mon Apr 15 09:52:04 UTC 2024 x86_64 x86_64 x86_64 GNU/Linux  21:37:52 up 7 days, 14:35,  1 user,  load average: 0.16, 1.29, 1.43

--- abstract-machine/am/src/riscv/nemu/cte.c
+++ abstract-machine/am/src/riscv/nemu/cte.c
@@ -23,7 +23,7 @@ Context* __am_irq_handle(Context *c) {
     c = user_handler(ev, c);
     assert(c != NULL);
   }
-	//printf("__am_irq_handle(): return now c = %#x\n", c);
+	printf("__am_irq_handle(): return now c = %#x\n", c);
   return c;
 }
 
