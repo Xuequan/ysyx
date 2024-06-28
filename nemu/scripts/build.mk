9a2f347790e73dae3ec4e46f8cf1a520ea6b594e
tracer-ysyx <tracer@ysyx.org>
2024-06-28 09:08:05 +0800

    > compile NEMU ysyx_22040000 廖学全 Linux chuan 5.15.0-113-generic #123-Ubuntu SMP Mon Jun 10 08:16:17 UTC 2024 x86_64 x86_64 x86_64 GNU/Linux  09:08:05 up 46 min,  1 user,  load average: 0.26, 0.37, 0.31

--- nemu/scripts/build.mk
+++ nemu/scripts/build.mk
@@ -23,7 +23,7 @@ CXX := g++
 endif
 LD := $(CXX)
 INCLUDES = $(addprefix -I, $(INC_PATH))
-INCLUDES += $(addprefix -L, $(LLVM_LIB))
+INCLUDES += $(addprefix -I, $(LLVM_LIB))
 CFLAGS  := -O2 -MMD -Wall -Werror $(INCLUDES) $(CFLAGS)
 LDFLAGS := -O2 $(LDFLAGS)
 
