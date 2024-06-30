bae1961ee51772d9701fdeb66586acc1d467183d
tracer-ysyx <tracer@ysyx.org>
2024-06-30 10:18:54 +0800

    > sim RTL ysyx_22040000 廖学全 Linux chuan 5.15.0-113-generic #123-Ubuntu SMP Mon Jun 10 08:16:17 UTC 2024 x86_64 x86_64 x86_64 GNU/Linux  10:18:54 up  1:23,  1 user,  load average: 1.54, 1.44, 1.05

--- npc/vsrc/ysyx_23060208_clint.v
+++ npc/vsrc/ysyx_23060208_clint.v
@@ -106,10 +106,12 @@ always @(posedge clk) begin
 		clint_rdata_r <= 0;
 	else if (next == SHAKED_AR && clint_araddr == clint_addr)
 		//clint_rdata_r <= dsram_read(clint_araddr);
-		clint_rdata_r <= mtime_r[31:0];
+		//clint_rdata_r <= mtime_r[31:0];
+		clint_rdata_r <= 32'b1;
 	else if (next == SHAKED_AR && clint_araddr == clint_addr + 4)
 		//clint_rdata_r <= dsram_read(clint_araddr);
-		clint_rdata_r <= mtime_r[63:32];
+		//clint_rdata_r <= mtime_r[63:32];
+		clint_rdata_r <= 32'b0;
 end
 
 endmodule
