ba769689fd45ed34d31f439507634f77e4961298
tracer-ysyx <tracer@ysyx.org>
2024-07-08 16:48:55 +0800

    > sim RTL ysyx_22040000 廖学全 Linux chuan 5.15.0-113-generic #123-Ubuntu SMP Mon Jun 10 08:16:17 UTC 2024 x86_64 x86_64 x86_64 GNU/Linux  16:48:55 up  8:14,  1 user,  load average: 0.86, 0.94, 0.64

--- abstract-machine/am/src/riscv/ysyxSoC/trm.c
+++ abstract-machine/am/src/riscv/ysyxSoC/trm.c
@@ -23,7 +23,25 @@ void halt(int code) {
 	}
 }
 
+extern char _rodata_start[];
+extern char _rodata_size[];
+extern char _rodata_load_start[];
+extern char _data_start[];
+extern char _data_size[];
+extern char _data_load_start[];
+extern char _bss_start[];
+extern char _bss_size[];
+extern char _bss_load_start[];
+void *memcpy(void*, const void *, size_t);
+void copy_data(void *dst, void *src, size_t n) {
+	if ((char *)dst != (char *)src) 
+		memcpy(dst, src, n); 
+}
 void _trm_init() {
+	copy_data(_rodata_start, _rodata_load_start, (size_t)_rodata_size);		
+	copy_data(_data_start, _data_load_start, (size_t)_data_size);		
+	copy_data(_bss_start, _bss_load_start, (size_t)_bss_size);		
+
   int ret = main(mainargs);
   halt(ret);
 }
