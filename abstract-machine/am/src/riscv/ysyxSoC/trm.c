6ccf051804939003235493f2b86e64829f517bca
tracer-ysyx <tracer@ysyx.org>
2024-07-13 11:03:00 +0800

    > sim RTL ysyx_22040000 廖学全 Linux chuan 5.15.0-113-generic #123-Ubuntu SMP Mon Jun 10 08:16:17 UTC 2024 x86_64 x86_64 x86_64 GNU/Linux  11:03:00 up 1 day,  2:35, 21 users,  load average: 1.29, 1.45, 1.45

--- abstract-machine/am/src/riscv/ysyxSoC/trm.c
+++ abstract-machine/am/src/riscv/ysyxSoC/trm.c
@@ -6,6 +6,7 @@
 #define UART_LC 3   // line control register
 #define UART_DL1 0  // divisor latch low byte
 #define UART_DL2 1  // divisor latch high byte
+#define UART_LS 5   // line status register 
 
 extern char _heap_start;
 extern char _heap_end;
@@ -26,8 +27,12 @@ void init_uart() {
 	*(volatile char *)(UART_BASE + UART_LC) = 0; // set lcr[7] 1
 
 }
+
 void putch(char ch) {
-	*(volatile char *)(UART_BASE + UART_TX) = ch;
+	char lsr = *(volatile char *)(UART_BASE + UART_LS);
+	int empty = lsr & 0b00100000;
+	if (empty)
+		*(volatile char *)(UART_BASE + UART_TX) = ch;
 }
 
 void halt(int code) {
