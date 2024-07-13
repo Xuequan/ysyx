07dc80a3adb2ffad6ff65857c04388ef633e8205
tracer-ysyx <tracer@ysyx.org>
2024-07-13 11:29:07 +0800

    > sim RTL ysyx_22040000 廖学全 Linux chuan 5.15.0-113-generic #123-Ubuntu SMP Mon Jun 10 08:16:17 UTC 2024 x86_64 x86_64 x86_64 GNU/Linux  11:29:07 up 1 day,  3:01, 21 users,  load average: 1.36, 1.23, 1.35

--- abstract-machine/am/src/riscv/ysyxSoC/trm.c
+++ abstract-machine/am/src/riscv/ysyxSoC/trm.c
@@ -21,7 +21,7 @@ static const char mainargs[] = MAINARGS;
 // init uart 
 void init_uart() {
 	*(volatile char *)(UART_BASE + UART_LC) = 0b10000011; // set lcr[7] 1
-	*(volatile char *)(UART_BASE + UART_DL1) = 0x09;
+	*(volatile char *)(UART_BASE + UART_DL1) = 0x60;
 	*(volatile char *)(UART_BASE + UART_DL2) = 0x00;
 
 	*(volatile char *)(UART_BASE + UART_LC) = 0b00000011; // set lcr[7] 0
