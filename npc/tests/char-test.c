f49ba852c21df3686d3569cab63013b12f779eac
tracer-ysyx <tracer@ysyx.org>
2024-07-19 11:14:04 +0800

    > sim RTL ysyx_22040000 廖学全 Linux chuan 5.15.0-116-generic #126-Ubuntu SMP Mon Jul 1 10:14:24 UTC 2024 x86_64 x86_64 x86_64 GNU/Linux  11:14:04 up  2:42, 31 users,  load average: 0.90, 1.12, 1.12

--- npc/tests/char-test.c
+++ npc/tests/char-test.c
@@ -1,6 +1,6 @@
 #define UART_BASE 0x10000000L
 #define UART_TX 0
-void _start() {
+__attribute__((__aligned__(4))) void char_test() {
 	*(volatile char *)(UART_BASE + UART_TX) = 'A';
 	*(volatile char *)(UART_BASE + UART_TX) = '\n';
 	while(1);
