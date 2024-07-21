7a11da9bfcbef1857b854529660b806230ce4a7c
tracer-ysyx <tracer@ysyx.org>
2024-07-21 15:16:10 +0800

    > sim RTL ysyx_22040000 廖学全 Linux chuan 5.15.0-116-generic #126-Ubuntu SMP Mon Jul 1 10:14:24 UTC 2024 x86_64 x86_64 x86_64 GNU/Linux  15:16:10 up  6:47,  9 users,  load average: 1.25, 0.96, 0.67

--- npc/csrc/dpi-c.cpp
+++ npc/csrc/dpi-c.cpp
@@ -9,41 +9,16 @@ extern "C" void psram_read(int32_t addr, int32_t *data) {
 	*data = vaddr_read(addr + 0x80000000, 4);
 }
 
-extern "C" void psram_write(int32_t addr, int32_t data) {    
-	*data = vaddr_write(addr + 0x80000000, 4, data);
+extern "C" void psram_write(int addr, int data) {    
+	uint32_t adr = (uint32_t)addr;
+	vaddr_write(adr + 0x80000000, 4, data);
 }
 
 extern "C" void flash_read(int32_t addr, int32_t *data) {    
 	*data = vaddr_read(addr + 0x30000000, 4);
 }
 extern "C" void mrom_read(int32_t addr, int32_t *data) {
-	//*data = 0x00100073; // ebreak inst
-	//int32_t ret = vaddr_read(addr, 4);
 	*data = vaddr_read(addr, 4);
-	//printf("mrom_read(): addr = %#x, return %#x\n", addr, *data);
-}
-
-/* 总是读取地址为 raddr & ~0x3u 的4字节返回  */
-extern "C" int isram_read(int raddr) {
-  return vaddr_ifetch(raddr, 4);
-}
-
-extern "C" int dsram_read(int raddr) {
-  return vaddr_read(raddr, 4);
-}
-
-extern "C" void dsram_write(int waddr, int wdata, char wmask) {
-	uint32_t addr = (uint32_t)waddr;
-	if ((uint8_t)wmask == 1 )
-  	return vaddr_write(addr, 1, wdata);
-	else if ((uint8_t)wmask == 2) 
-  	return vaddr_write(addr, 2, wdata);
-	else if ((uint8_t)wmask == 4) 
-  	return vaddr_write(addr, 4, wdata);
-	else {
-		printf("Error, wmask = %#x error\n", (uint8_t)wmask);
-		return;
-	}
 }
 
 //extern void check_if_ebreak(svBit* o);
--- npc/csrc/dpi-c.h
+++ npc/csrc/dpi-c.h
@@ -6,24 +6,10 @@
 #include "VysyxSoCFull__Dpi.h"
 
 extern "C" void psram_read(int32_t addr, int32_t *data); 
-extern "C" void psram_write(int32_t addr, int32_t data); 
+extern "C" void psram_write(int addr, int data); 
 
 extern "C" void flash_read(int32_t addr, int32_t *data); 
 extern "C" void mrom_read(int32_t addr, int32_t *data); 
-
-word_t vaddr_ifetch(vaddr_t addr, int len);
-
-// 取指令
-// 总是读取地址为 raddr & ~0x3u 的4字节返回
-// raddr 是 vaddr 
-extern "C" int isram_read(int raddr);
-
-// load instruction
-extern "C" int dsram_read(int load_addr);
-  // 总是往地址为 'waddr & ~0x3u '的4字节按写掩码 'wmask' 写入 'wdata'
-  // 'wmask' 中每比特表示 'wdata' 中1个字节的掩码，
-  // 如 'wmask = 0x3' 代表只写入最低2个字节，内存中的其它字节保持不>变
-extern "C" void dsram_write(int waddr, int wdata, char wmask);
 
 // below from ysyxSoCFull_IDU
 //extern void check_if_ebreak(svBit* o);
