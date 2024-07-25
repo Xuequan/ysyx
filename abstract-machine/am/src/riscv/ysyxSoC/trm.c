f587f0d84b121558a71aecd2d5e40607fe887c75
tracer-ysyx <tracer@ysyx.org>
2024-07-25 18:03:26 +0800

    > sim RTL ysyx_22040000 廖学全 Linux chuan 5.15.0-116-generic #126-Ubuntu SMP Mon Jul 1 10:14:24 UTC 2024 x86_64 x86_64 x86_64 GNU/Linux  18:03:26 up  9:39, 14 users,  load average: 0.60, 0.80, 0.79

--- abstract-machine/am/src/riscv/ysyxSoC/trm.c
+++ abstract-machine/am/src/riscv/ysyxSoC/trm.c
@@ -66,17 +66,18 @@ extern char _data_end[];
 extern char _bss_start[];
 extern char _bss_end[];
 
+/*
 void __attribute__  ((section (".first_init"))) _data_init() {
 	char *dst;
-	/* Zero bss */
+	// Zero bss
 	for (dst = _bss_start; dst < _bss_end; dst++)
 		*dst = 0;
 
 	char *src = _data_Load_addr;
 	dst = _data_start;
-	/* copy '.data' srction to sram  */
+	// copy '.data' srction to sram  
 	while (dst < _data_end)
 		*dst++ = *src++;
-
 	_trm_init();
 }
+*/
