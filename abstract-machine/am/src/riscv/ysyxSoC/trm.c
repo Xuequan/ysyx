9604e43536a2dd4ece581c424f4632ebca14be3d
tracer-ysyx <tracer@ysyx.org>
2024-07-26 10:46:23 +0800

    > sim RTL ysyx_22040000 廖学全 Linux chuan 5.15.0-116-generic #126-Ubuntu SMP Mon Jul 1 10:14:24 UTC 2024 x86_64 x86_64 x86_64 GNU/Linux  10:46:23 up  2:08, 14 users,  load average: 1.20, 1.10, 0.90

--- abstract-machine/am/src/riscv/ysyxSoC/trm.c
+++ abstract-machine/am/src/riscv/ysyxSoC/trm.c
@@ -9,11 +9,10 @@
 #define UART_LS 5   // line status register 
 
 extern char _heap_start;
-//extern char _heap_end;
+extern char _heap_end;
 int main(const char *args);
 
-//Area heap = RANGE(&_heap_start, &_heap_end);
-Area heap = RANGE(&_heap_start, &_heap_start + 0x1000);
+Area heap = RANGE(&_heap_start, &_heap_end);
 #ifndef MAINARGS
 #define MAINARGS ""
 #endif
