294da156faf179559fca72ff0e24ddd189e7facf
tracer-ysyx <tracer@ysyx.org>
2024-08-01 12:17:27 +0800

    > sim RTL ysyx_22040000 廖学全 Linux chuan 5.15.0-117-generic #127-Ubuntu SMP Fri Jul 5 20:13:28 UTC 2024 x86_64 x86_64 x86_64 GNU/Linux  12:17:27 up  3:56, 22 users,  load average: 0.91, 1.12, 1.16

--- npc/csrc/dpi-c.cpp
+++ npc/csrc/dpi-c.cpp
@@ -23,13 +23,13 @@ extern "C" void sdram_write(int addr, int data, char mask) {
    * 而传入的数据就是要写入的数据，并没有做处理
    * 因此，这里用 mask 合理
    */
-  if (addr = FULL_SIZE/16) printf("1/16 has finished \n");
-  if (addr = FULL_SIZE/8)  printf("1/8 has finished \n");
-  if (addr = FULL_SIZE/4)  printf("1/4 has finished \n");
-  if (addr = FULL_SIZE/2)  printf("1/2 has finished \n");
-  if (addr = FULL_SIZE/2 + FULL_SIZE/16)  printf("1/2 + 1/16 has finished \n");
-  if (addr = FULL_SIZE/2 + FULL_SIZE/8)  printf("1/2 + 1/8 has finished \n");
-  if (addr = FULL_SIZE/2 + FULL_SIZE/4)  printf("1/2 + 1/4 has finished \n");
+  if (addr == FULL_SIZE/16) printf("1/16 has finished \n");
+  if (addr == FULL_SIZE/8)  printf("1/8 has finished \n");
+  if (addr == FULL_SIZE/4)  printf("1/4 has finished \n");
+  if (addr == FULL_SIZE/2)  printf("1/2 has finished \n");
+  if (addr == FULL_SIZE/2 + FULL_SIZE/16)  printf("1/2 + 1/16 has finished \n");
+  if (addr == FULL_SIZE/2 + FULL_SIZE/8)  printf("1/2 + 1/8 has finished \n");
+  if (addr == FULL_SIZE/2 + FULL_SIZE/4)  printf("1/2 + 1/4 has finished \n");
 
 	uint32_t waddr = (uint32_t)addr + 0xa0000000;
 	int len = 0;
@@ -77,13 +77,13 @@ extern "C" void sdram_write(int addr, int data, char mask) {
 }
 
 extern "C" void sdram_read(int32_t addr, int32_t *data) {    
-  if (addr = FULL_SIZE/16) printf("1/16 has finished \n");
-  if (addr = FULL_SIZE/8)  printf("1/8 has finished \n");
-  if (addr = FULL_SIZE/4)  printf("1/4 has finished \n");
-  if (addr = FULL_SIZE/2)  printf("1/2 has finished \n");
-  if (addr = FULL_SIZE/2 + FULL_SIZE/16)  printf("1/2 + 1/16 has finished \n");
-  if (addr = FULL_SIZE/2 + FULL_SIZE/8)  printf("1/2 + 1/8 has finished \n");
-  if (addr = FULL_SIZE/2 + FULL_SIZE/4)  printf("1/2 + 1/4 has finished \n");
+  if (addr == FULL_SIZE/16) printf("1/16 has finished \n");
+  if (addr == FULL_SIZE/8)  printf("1/8 has finished \n");
+  if (addr == FULL_SIZE/4)  printf("1/4 has finished \n");
+  if (addr == FULL_SIZE/2)  printf("1/2 has finished \n");
+  if (addr == FULL_SIZE/2 + FULL_SIZE/16)  printf("1/2 + 1/16 has finished \n");
+  if (addr == FULL_SIZE/2 + FULL_SIZE/8)  printf("1/2 + 1/8 has finished \n");
+  if (addr == FULL_SIZE/2 + FULL_SIZE/4)  printf("1/2 + 1/4 has finished \n");
 	uint32_t raddr = (uint32_t)addr + 0xa0000000;
 	*data = vaddr_read(raddr, 4);
 #ifdef DO_PRINT
