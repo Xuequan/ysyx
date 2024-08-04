109eeced932f23a78cab0bc5332314bb8533c890
tracer-ysyx <tracer@ysyx.org>
2024-08-04 21:40:51 +0800

    > compile NEMU ysyx_22040000 廖学全 Linux chuan 5.15.0-117-generic #127-Ubuntu SMP Fri Jul 5 20:13:28 UTC 2024 x86_64 x86_64 x86_64 GNU/Linux  21:40:51 up 2 days, 12:25, 12 users,  load average: 1.59, 1.87, 2.16

--- nemu/src/monitor/monitor.c
+++ nemu/src/monitor/monitor.c
@@ -27,6 +27,7 @@ void init_disasm(const char *triple);
 void init_elf();
 void init_mrom();
 void init_sram();
+void init_sdram();
 
 static void welcome() {
   Log("Trace: %s", MUXDEF(CONFIG_TRACE, ANSI_FMT("ON", ANSI_FG_GREEN), ANSI_FMT("OFF", ANSI_FG_RED)));
@@ -126,6 +127,9 @@ void init_monitor(int argc, char *argv[]) {
 
 	/* Initialize sram */
 	init_sram();
+
+  /* Initialize sdram */
+  init_sdram();
 
   /* Initialize devices. */
   IFDEF(CONFIG_DEVICE, init_device());
