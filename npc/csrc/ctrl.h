47fabcc3859c8d540ca66b2bcd1e53e650e378b7
tracer-ysyx <tracer@ysyx.org>
2024-08-04 21:56:56 +0800

    > sim RTL ysyx_22040000 廖学全 Linux chuan 5.15.0-117-generic #127-Ubuntu SMP Fri Jul 5 20:13:28 UTC 2024 x86_64 x86_64 x86_64 GNU/Linux  21:56:56 up 2 days, 12:41, 12 users,  load average: 1.59, 1.67, 1.89

--- npc/csrc/ctrl.h
+++ npc/csrc/ctrl.h
@@ -1,11 +1,11 @@
 #ifndef __CTRL_H__
 #define __CTRL_H__
 
-/* control difftest switch, monitor.cpp & npc-exec.cpp */
+/* control difftest, monitor.cpp & npc-exec.cpp */
 #define DIFFTEST 1
 
 /* control switch which will print sdram write/read information. dpi-c.cpp*/
-//#define DO_PRINT 1
+#define DO_PRINT 1
 
 /* control switch which will print psram write/read information. dpi-c.cpp*/
 //#define DO_PRINT_PSRAM 
