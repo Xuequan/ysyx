91a63943eb00ed301316815fb3e05d72f8889a19
tracer-ysyx <tracer@ysyx.org>
2024-08-04 10:06:52 +0800

    > sim RTL ysyx_22040000 廖学全 Linux chuan 5.15.0-117-generic #127-Ubuntu SMP Fri Jul 5 20:13:28 UTC 2024 x86_64 x86_64 x86_64 GNU/Linux  10:06:52 up 2 days, 51 min, 14 users,  load average: 1.57, 1.70, 1.58

--- npc/csrc/ctrl.h
+++ npc/csrc/ctrl.h
@@ -5,16 +5,16 @@
 #define DIFFTEST 1
 
 /* control switch which will print sdram write/read information. dpi-c.cpp*/
-#define DO_PRINT 1
+//#define DO_PRINT 1
 
 /* control switch which will print psram write/read information. dpi-c.cpp*/
 //#define DO_PRINT_PSRAM 
 
 /* control log_wirte */
-//#define LOG_WRITE_ENABLE 1
+#define LOG_WRITE_ENABLE 1
 
 /* connect NVBoard */
-#define CONNECT_NVBOARD 1
+//#define CONNECT_NVBOARD 1
 
 
 #endif
