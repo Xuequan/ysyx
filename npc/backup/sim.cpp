dcd3af399f291e24cbef8e590abd21e697020136
Xuequan <15623564877@163.com>
2024-08-06 12:59:40 +0800

    第一次Push 到仓库，保存下。

---
+++ npc/backup/sim.cpp
@@ -0,0 +1,133 @@
+#include <cstdint>
+#include "VysyxSoCFull.h"
+#include "verilated_vcd_c.h"
+#include "VysyxSoCFull__Dpi.h"
+#include "VysyxSoCFull___024root.h"
+#include "dpi-c.h"
+#include "nvboard.h"
+
+static VysyxSoCFull* top;
+static VerilatedContext* contextp;
+static VerilatedVcdC* tfp;
+
+void nvboard_bind_all_pins(VysyxSoCFull* top);
+void nvboard_quit();
+
+static void step_and_dump_wave() {
+	top->eval();
+	// generate wave file
+#ifdef WAVE_FILE
+	contextp->timeInc(1);
+	tfp->dump(contextp->time());
+#endif
+}
+
+// execute one cycle
+static void sim_one_cycle() {
+	for(int i = 0; i < 2; i++) {
+		top->clock ^= 1;
+		step_and_dump_wave();
+    nvboard_update();
+	}
+}
+
+// execute one inst
+// return 0 --- no sepcial
+// return 1 ---> function call 
+// return 2 ---> function ret
+// return 3 ---> ebreak_inst
+int sim_once() {
+	int ret = 0;
+	while ( check_exu_ready_go() != true ) {
+
+		if (check_access_fault_ifu() ) {
+			printf("IFU 'pc' = '%#x' Access fault! Please check!\n", get_pc() );
+			return 0;
+		}
+
+		if (inst_is_ebreak()){
+			ret = 3;
+		}else if (inst_is_jal()){
+			ret = 1;
+		}else if (inst_is_jalr()){
+			if (rs1() == 1 && rd() == 0) 
+				ret = 2;
+			else if(rs1() == 1 && rd() == 1)
+				ret = 2;
+			else if(rs1() == 6 && rd() == 0)
+				ret = 2;
+			else 
+				ret = 1;
+		}
+		sim_one_cycle();
+	} // end-while
+
+	if (check_access_fault_exu() ) {
+		printf("EXU 'pc' = '%#x' Access fault! Please check!\n", get_pc() );
+		return 0;
+	}
+		
+	sim_one_cycle();
+	return ret; 
+}
+
+void sim_init() {
+ 	contextp = new VerilatedContext;
+	tfp = new VerilatedVcdC;
+	top = new VysyxSoCFull;
+
+  nvboard_bind_all_pins(top);
+  nvboard_init();
+
+	contextp->traceEverOn(true);
+#ifdef WAVE_FILE
+	top->trace(tfp, 0);
+  tfp->open("dump.vcd");
+#endif
+
+	// initial reset
+	int i = -1;
+	while ( i < 29) {
+		i++;
+		top->clock ^= 1;
+		top->reset = 1;
+		step_and_dump_wave();
+	}
+	top->reset = 0;
+	top->clock ^= 1;
+	step_and_dump_wave();
+}
+
+void sim_exit() {
+#ifdef WAVE_FILE
+	tfp->close();
+	delete tfp;
+#endif
+	delete top;
+	delete contextp;
+
+  nvboard_quit();
+}
+
+uint32_t get_clock_from_top(){
+	return top->clock;
+}
+// from arch.cpp
+extern const char *regs[];
+extern uint32_t npc_regs[16];
+extern uint32_t update_reg_no();
+extern uint32_t update_reg_data();
+void get_npc_regs() {
+	uint32_t* ptr = NULL;
+	ptr = (top->rootp->ysyxSoCFull__DOT__asic__DOT__cpu__DOT__cpu__DOT__idu__DOT__regfile__DOT__rf).data();
+	for(int i = 0; i < 16; i++){
+		npc_regs[i] = ptr[i];
+	}
+}
+
+void isa_reg_display() {
+	get_npc_regs();
+  for( int i = 0; i < 16; i++){
+		printf("%s: %#x\n", regs[i], npc_regs[i]);
+  }
+}
---
+++ npc/csrc/ctrl.h
@@ -0,0 +1,31 @@
+#ifndef __CTRL_H__
+#define __CTRL_H__
+
+/* control difftest, monitor.cpp & npc-exec.cpp */
+#define DIFFTEST 1
+
+/* control switch which will print sdram write/read information. dpi-c.cpp*/
+#define DO_PRINT 1
+
+/* control switch which will print psram write/read information. dpi-c.cpp*/
+//#define DO_PRINT_PSRAM 
+
+/* control log_wirte */
+//#define LOG_WRITE_ENABLE 1
+
+/* control write every instruction into log */  
+//#define WRITE_EVERY_INST 1
+
+
+/* connect NVBoard */
+//#define CONNECT_NVBOARD 1
+
+
+/* print flash memory and other memory space at the beging of program */  
+// #define PRINT_FLASH_MEM 1
+
+/* control whether generating wave file */
+//#define WAVE_FILE 1
+
+
+#endif
